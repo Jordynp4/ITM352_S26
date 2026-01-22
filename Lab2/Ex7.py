a95d27f68849aabe7c18502795e8b7dfec4faa51
Jordynp4 <jordynp4@hawaii.edu>
2026-01-22 12:56:25 -1000

    Create Ex7.py

---
+++ Lab2/Ex7.py
@@ -0,0 +1,12 @@
+# ake the user to enter a temperature in fahrenheit.
+# convert the temperature to celsius using the formula: C = (F - 32) * 5/9
+# Name: Jordyn Pendergrass
+# Date: Jan 22, 2026
+
+fahrenheit_input = input("Please enter a temperature in Fahrenheit: ")
+fahrenheit_value = float(fahrenheit_input)
+celsius_value = (fahrenheit_value - 32) * 5 / 9
+celsius_value_rounded = round(celsius_value, 1)
+
+print("You entered:", fahrenheit_value)
+print(f"The temperature in Celsius is: {celsius_value_rounded}")
