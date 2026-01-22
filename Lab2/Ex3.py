56aba08b1d5d81e89eba5adf00899e2b309d5c91
Jordynp4 <jordynp4@hawaii.edu>
2026-01-22 12:28:13 -1000

    Update Ex3.py

--- Lab2/Ex3.py
+++ Lab2/Ex3.py
@@ -7,5 +7,8 @@ input_value = input("Please enter a floating point number: ")
 float_value = float(input_value)
 squared_value = float_value ** 2
 
+# Round the number to 2 decimal places
+squared_value = round(squared_value, 2)
+
 print("You entered:", float_value)
 print(f"The square value is: {squared_value}")
