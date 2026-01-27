37d7925bf68d267a0adc10f3f8e7011e90ff6de6
Jordynp4 <jordynp4@hawaii.edu>
2026-01-27 13:00:55 -1000

    updated Ex1.py

--- Lab4/Ex1.py
+++ Lab4/Ex1.py
@@ -1,3 +1,11 @@
-import cryptography
+from cryptography.fernet import Fernet
 
-print(cryptography.__version__)
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+encode_text = cipher_suite.encrypt(b"This is a really secret message")
+print(f"Encoded text: {encode_text}")
+
+# use the cryptography library to encode and ecode a message
+decoded_text = cipher_suite.decrypt(encode_text)
+print(f"Decoded text: {decoded_text.decode('utf-8')}")
