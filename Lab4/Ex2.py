ba6606a410c13239edfe4c69dd3587c657a95c20
Jordynp4 <jordynp4@hawaii.edu>
2026-02-01 22:22:24 -1000

    update

---
+++ Lab4/Ex2.py
@@ -0,0 +1,8 @@
+# define a list of survey respones values
+# define a tuple of ID values 
+# append the tuple to the list. Print the list.
+
+survey_responses = [5, 7, 3, 8]
+id_values = (1012, 1035, 1021, 1053)
+survey_responses.append(id_values)
+print("Survey responses with IDs:", survey_responses)
