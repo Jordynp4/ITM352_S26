9c1dff611f8fc79543c5721d4560e6c2fba9249e
Jordynp4 <jordynp4@hawaii.edu>
2026-02-10 12:59:36 -1000

    Update Ex1.py

--- Lab6/Ex1.py
+++ Lab6/Ex1.py
@@ -2,11 +2,11 @@ emotions = ("sad", "angry", "fear", "surprise", "happy")
 
 # Write code that uses a conditional expression (do not use an if-statement or ternary expression) to print “true” if the last element is “happy” and there are more than 3 elements, or “false” if it is not
 
-#result = emotions[-1] == "happy" and len(emotions) > 3
+result = emotions[-1] == "happy" and len(emotions) > 3
 #print(result)
 
 # rewrite the above code using an if-statement instead of a conditional expression
-if emotions[-1] == "happy" and len(emotions) > 3:
+if(result == True):
     print("true")
 else:
     print("false")
