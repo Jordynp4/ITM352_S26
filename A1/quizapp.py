9ce442d7221453b65c3612414aefe38865f6135d
Jordynp4 <jordynp4@hawaii.edu>
2026-03-05 13:03:19 -1000

    Update quizapp.py

--- A1/quizapp.py
+++ A1/quizapp.py
@@ -1,8 +1,11 @@
 # Name: Jordyn Pendergrass
 # Date: March 5, 2026
 
-# Create a quiz application that asks the user 5 multiple-choice questions. Each question should have 4 options, and the user should input their answer (A, B, C, or D). 
+# Create a quiz application that asks the user 5 multiple-choice questions. Each question should have 4 options.
+# Allow the user to select correct answer by a label. 
 # At the end of the quiz, the application should calculate and display the user's final score as a percentage.
+
+from string import ascii_lowercase
 
 QUESTIONS = { 
     "What kind of magical creature is Shrek?": ["Ogre", "Giant", "Troll","Centaur"],
@@ -12,14 +15,27 @@ QUESTIONS = {
     "What is the name of the giant ginerbread man in Shrek 2?": ["Mongo", "Gingy", "gum drop", "cookie"]
 }
 
-
-for question, options in QUESTIONS.items():
+score = 0
+for num, (question, options) in enumerate(QUESTIONS.items(), start=1):
+    print(f"Question {num}:")
+    print(question)
     correct_answer = options[0]  # The first option is the correct answer
-    for alternative in sorted(options):
-        print(f"  - {alternative}")
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(options)))
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}. {alternative}")
+    
+    # input answer validation loop
+    while True:
+        answer_label = input("Enter answer here: ")
+        if answer_label in labeled_alternatives:
+            break
+        print("Invalid input. Please enter a valid choice.")
 
-    answer = input(question + ": ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
         print("Correct!")
+        score += 1
     else:
-        print(f"The answer is '{correct_answer}' not '{answer}'")
+        print(f"Incorrect!")
+
+print(f"\nYour final score is {score}/{len(QUESTIONS)} ({score/len(QUESTIONS)*100:.1f}%)")
