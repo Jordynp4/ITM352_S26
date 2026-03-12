749afaeef7d87e9cdd690574436b4f178bf7ccd6
Jordynp4 <jordynp4@hawaii.edu>
2026-03-12 12:14:25 -1000

    update

--- Lab9/Ex1e.py
+++ Lab9/Ex1e.py
@@ -1,5 +1,10 @@
-file_object = open("names.txt")
-contents_list = file_object.readlines()
-print(contents_list)
-print(f"Number of names: {len(contents_list)}")
-file_object.close()
+# Append a new name at the end of the file.
+
+with open("names.txt") as file_object:
+    contents_list = file_object.readlines()
+    print(contents_list)
+    
+
+with open("names.txt", "a") as file_object:
+    file_object.write("Williams, Venus\n") 
+    print(f"Number of names: {len(contents_list)}")
