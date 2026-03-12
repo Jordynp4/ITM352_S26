558d39a9fee28010f2d83b4fbc49886be31611cb
Jordynp4 <jordynp4@hawaii.edu>
2026-03-11 23:14:48 -1000

    update

--- Lab8B/Ex6.py
+++ Lab8B/Ex6.py
@@ -2,10 +2,10 @@
 # the number if it is not equal to 5 (using continue) and stop the 
 # loop entirely and print a message when it reaches 8 (using break).
 
-for x in range(10):
-    if x != 5:
-        print(x)
+for x in range(1,11):
+    if x == 5:
         continue
     if x == 8:
         print("done!")
         break
+    print(x)
