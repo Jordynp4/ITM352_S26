f1dfdffb1390cce422fa6920b852a9251611d26f
Jordynp4 <jordynp4@hawaii.edu>
2026-03-12 12:20:42 -1000

    update

--- Lab9/Ex1e.py
+++ Lab9/Ex1e.py
@@ -6,5 +6,7 @@ with open("names.txt") as file_object:
     
 
 with open("names.txt", "a") as file_object:
+    print("appending new name to the file...")
     file_object.write("Williams, Venus\n") 
+    contents_list.append("Williams, Venus\n")
     print(f"Number of names: {len(contents_list)}")
