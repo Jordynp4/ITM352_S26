36683a22861baf07493bbca8627c612d37020f18
Jordynp4 <jordynp4@hawaii.edu>
2026-03-24 12:57:27 -1000

    update

---
+++ Lab10A/Ex4.py
@@ -0,0 +1,12 @@
+# Read a json file of taxi trip data and create a dataframe.
+# Calculate the median fare.
+
+import json
+import pandas as pd
+
+taxi_df = pd.read_json("taxi_trips.json")
+print(taxi_df.describe())
+
+print(taxi_df.head(0))
+median_fare = taxi_df["fare"].median()
+print(f"Median fare: ${median_fare:.2f}")
