176bc798d90c022be7f23fa9acabd212a8943986
Jordynp4 <jordynp4@hawaii.edu>
2026-03-31 12:59:00 -1000

    update

--- Lab11/Ex1.py
+++ Lab11/Ex1.py
@@ -1,14 +1,16 @@
-
-
+# read in a CSV file with sales data, called sales_data.csv into a dataframe
+# print the first 5 rows and show the data types of the columns
 
 import pandas as pd
 import numpy as np
+import pyarrow
 
 filename = 'Sales_data.csv'
 
 pd.set_option('display.max_columns', None) #show all columns in the output
 
 df = pd.read_csv(filename, engine='pyarrow')
+df['order_date'] = pd.to_datetime(df['order_date'], errors='coerce')
 
 print(df.info())
 print(df.describe())
---
+++ Lab11/Ex2.py
@@ -0,0 +1,27 @@
+# read in a CSV file and create a dataframe
+# pivot the dataframe, aggregate sales by region, columns defined by order type and totals.
+
+import pandas as pd
+import numpy as np
+import pyarrow
+
+filename = 'Sales_data.csv'
+
+pd.set_option('display.max_columns', None) #show all columns in the output
+
+df = pd.read_csv(filename, engine='pyarrow')
+df['order_date'] = pd.to_datetime(df['order_date'], format='%m/%d/%y', errors='coerce')
+
+#coerce quantity and unit_price to numeric, setting errors to null
+df['quantity'] = pd.to_numeric(df['quantity'], errors='coerce')
+df['unit_price'] = pd.to_numeric(df['unit_price'], errors='coerce')
+df['sales'] = df['quantity'] * df['unit_price'] # Calculate sales as quantity multiplied by unit price
+
+pivot_table = pd.pivot_table(df,
+                             index= 'sales_region',
+                             values= 'sales',
+                             columns= 'order_type',
+                             aggfunc= np.sum,
+                             margins= True,
+                             margins_name= 'Total_Sales')
+print(pivot_table)
