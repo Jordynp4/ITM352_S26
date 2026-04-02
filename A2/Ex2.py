5a90030bc8c32b09a78f2f190651c3b8c9bcf5bb
Jordynp4 <jordynp4@hawaii.edu>
2026-04-02 12:40:28 -1000

    Update Ex2.py

--- A2/Ex2.py
+++ A2/Ex2.py
@@ -17,6 +17,18 @@ def load_csv(filepath):
         end_time = time.time()
         load_time = end_time - start_time
         print(f"CSV file loaded successfully in {load_time:.2f} seconds.")
+        print(f"number of rows: {len(df)}")
+        print(f"columns: {df.columns.tolist()}")
+
+        required_columns = ['quantity', 'unit_price', 'order_date']
+        # Check if all required columns are present
+        missing_columns = [col for col in required_columns if col not in df.columns]
+        if missing_columns:
+            print(f"Warning: Missing required columns: {missing_columns}")
+        else:
+            print("All required columns are present.")
+
+        
         return df
     
     except Exception as e:
@@ -25,8 +37,8 @@ def load_csv(filepath):
 
 
 # Call load_csv to load the data amd print the first 10 rows
-#filename = 'Sales_data.csv'
-filename = 'Sales_data_test.csv'
+filename = 'Sales_data.csv'
+#filename = 'Sales_data_test.csv'
 sales_data = load_csv(filename)
 
 print(sales_data.head(10))
